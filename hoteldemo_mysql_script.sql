6e69b57309b21175b0eb71252b3a98b4cbbc60ff
anilpaduchuri <32687608+anilpaduchuri@users.noreply.github.com>
2018-04-20 10:24:13 -0400

    Update hoteldemo_mysql_script.sql
    modified

--- hoteldemo_mysql_script.sql
+++ hoteldemo_mysql_script.sql
@@ -31,6 +31,29 @@ values ("Hotel Deca - A Noble House Hotel","Burlingame","4507 Brooklyn Avenue NE
 insert into hotelcatalog(hotelname,city,address1,stateProvinceCode,countryCode,airportCode,locationDescription,amenityMask,postalCode,rateCurrencyCode,hotelId,rating,tripAdvisorRating,tripAdvisorRatingUrl,review,roomtype,availablerooms,price) 
 values ("Sheraton Seattle Hotel","New York","1400 6th Ave","WA","US","SEA","In Seattle (University District)",262147,"98101","USD",164116,4,4,"http://www.tripadvisor.com/img/cdsi/img2/ratings/traveler/4.0-12345-4.gif","high end hotel","deluxe",10,350.0);
 
+insert into hotelcatalog(hotelname,city,address1,stateProvinceCode,countryCode,airportCode,locationDescription,amenityMask,postalCode,rateCurrencyCode,hotelId,rating,tripAdvisorRating,tripAdvisorRatingUrl,review,roomtype,availablerooms,price) 
+values ("Motif Seattle","Seattle","1415 5th Ave","WA","US","SEA","Near Pike Place Market",7798786,"98121","USD",1257278,3,3,"http://www.tripadvisor.com/img/cdsi/img2/ratings/traveler/3.5-12345-4.gif","nice hotel","luxury",40,300.0);
+
+insert into hotelcatalog(hotelname,city,address1,stateProvinceCode,countryCode,airportCode,locationDescription,amenityMask,postalCode,rateCurrencyCode,hotelId,rating,tripAdvisorRating,tripAdvisorRatingUrl,review,roomtype,availablerooms,price) 
+values ("The Westin Seattle","Seattle","1900 5th Ave","WA","US","SEA","Near Pike Place Market",7798786,"98109","USD",164122,4,4,"http://www.tripadvisor.com/img/cdsi/img2/ratings/traveler/4.0-12345-4.gif","nice hotel","luxury",20,500.0);
+
+
+insert into hotelcatalog(hotelname,city,address1,stateProvinceCode,countryCode,airportCode,locationDescription,amenityMask,postalCode,rateCurrencyCode,hotelId,rating,tripAdvisorRating,tripAdvisorRatingUrl,review,roomtype,availablerooms,price) 
+values ("W Seattle","Seattle","1112 4th Ave","WA","US","SEA","Near Pike Place Market",7798786,"98101","USD",213497,4,4,"http://www.tripadvisor.com/img/cdsi/img2/ratings/traveler/4.0-12345-4.gif","nice hotel","luxury",25,600.0);
+
 insert into hotelreview(hotelid,reviewdesc) values (119562,"review about hotel test");
 insert into hotelreview(hotelid,reviewdesc) values (119562,"Nice  to stay at Hotel Andra");
 insert into hotelreview(hotelid,reviewdesc) values (164116,"review about Sheraton Seattle Hotel");
+
+insert into hotelreview(hotelid,reviewdesc) values (1257278,"happy to stay in this hotel");
+insert into hotelreview(hotelid,reviewdesc) values (1257278,"Nice  to stay at this hotel");
+insert into hotelreview(hotelid,reviewdesc) values (1257278,"review about Motif Seattle Hotel");
+
+insert into hotelreview(hotelid,reviewdesc) values (164122,"happy to stay in this hotel");
+insert into hotelreview(hotelid,reviewdesc) values (164122,"Nice  to stay at this hotel");
+insert into hotelreview(hotelid,reviewdesc) values (164122,"review about The Westin Seattle");
+
+
+insert into hotelreview(hotelid,reviewdesc) values (213497,"happy to stay in this hotel");
+insert into hotelreview(hotelid,reviewdesc) values (213497,"Nice  to stay at this hotel");
+insert into hotelreview(hotelid,reviewdesc) values (213497,"review about W Seattle");
